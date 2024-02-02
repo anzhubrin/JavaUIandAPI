894218ec4d3e69c1566ac7c48ffd97c6fa7806cf
anzhubrin <1111>
2024-02-02 09:14:25 +0000

    Refactoring

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -17,6 +18,7 @@ public class LoginTests {
         $("#user-name").setValue(standardUser);
         $("#password").setValue(password);
         $("#login-button").click();
+        $(".title").shouldHave(Condition.exactText("Products"));
 
     }
 }
