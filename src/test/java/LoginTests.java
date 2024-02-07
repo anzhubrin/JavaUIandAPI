2a04d188a468eba1c1da8ff5352ed542c17fd6ed
anzhubrin <1111>
2024-02-07 15:29:07 +0000

    refactor all tests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,50 +1,21 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 public class LoginTests {
 
     String standardUser = "standard_user";
     String password = "secret_sauce";
 
-    String BASE_URL = "https://www.saucedemo.com/";
-
     @Test
     void loginStandardUser() {
 
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        open(BASE_URL);
+        open("https://www.saucedemo.com/");
         $("#user-name").setValue(standardUser);
         $("#password").setValue(password);
         $("#login-button").click();
         $(".title").shouldHave(Condition.exactText("Products"));
 
-    }
-
-    @Test
-    void loginStandardUserWithStep() {
-
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        step("Открываем страницу авторизации", () -> {
-            open(BASE_URL);
-        });
-
-        step("Вводим логин и пароль пользователя", () -> {
-            $("#user-name").setValue(standardUser);
-            $("#password").setValue(password);
-        });
-
-        step("Нажимаем кнопку Login", () -> {
-            $("#login-button").click();
-        });
-
-        step("Проверяем, что авторизация прошла успешно", () -> {
-            $(".title").shouldHave(Condition.exactText("Products"));
-        });
     }
 }
