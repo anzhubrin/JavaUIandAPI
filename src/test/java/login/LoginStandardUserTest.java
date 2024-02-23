8a7983f53cd828b6bacca530a43532c10fb04250
anzhubrin <1111>
2024-02-23 13:51:40 +0000

    refactor build yaml

--- src/test/java/login/LoginStandardUserTest.java
+++ src/test/java/login/LoginStandardUserTest.java
@@ -1,7 +1,9 @@
 package login;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.LoginPage;
+
 public class LoginStandardUserTest {
 
     LoginPage loginPage = new LoginPage();
@@ -9,6 +11,9 @@ public class LoginStandardUserTest {
     @DisplayName("Авторизация стандартного пользователя")
     @Test
     void loginStandardUser() {
+
+        Configuration.remote = "http://localhos:4444/wd/hub";
+
         loginPage.openLoginPage();
         loginPage.setStandardLogin();
         loginPage.setPassword();
